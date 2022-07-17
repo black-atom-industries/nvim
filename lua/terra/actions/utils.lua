db99c23a7603473c0da4623370b8707bb2490124
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-07-17 14:30:51 +0200

    actions: introduce ccall()

--- lua/terra/actions/utils.lua
+++ lua/terra/actions/utils.lua
@@ -7,4 +7,19 @@ M.capitalize = function(str)
 	return string.gsub(" " .. str, "%W%l", string.upper):sub(2)
 end
 
+---Calls a function conditionally
+---@param condition boolean
+---@param cb_true function
+---@param cb_false? function
+---@return nil
+M.ccall = function(condition, cb_true, cb_false)
+	if condition then
+		cb_true()
+	else
+		if cb_false then
+			cb_false()
+		end
+	end
+end
+
 return M
