b773782021132af4982bf3f1228593f95c9b83a3
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2022-07-31 21:10:52 +0200

    stylua: add styluaignore and format types.lua

--- lua/terra/types.lua
+++ lua/terra/types.lua
@@ -1,47 +1,50 @@
----@alias Season "spring" | "summer" | "fall" | "winter"
----@alias Time "day" | "night"
----@alias CodeStyleFormat HighlightFormat
+----------------------------------------Highlight
+---@alias HighlightGroupName                      string
 
----@class TerraConfig.CodeStyle
----@field comments CodeStyleFormat
----@field keywords CodeStyleFormat
----@field functions CodeStyleFormat
----@field strings CodeStyleFormat
----@field variables CodeStyleFormat
-
----@class TerraConfig.Diagnostics
----@field darker boolean
----@field undercurl boolean
----@field background boolean
-
----@class TerraConfig
----@field season Season
----@field time Time
----@field icons table
----@field select_season string
----@field transparent boolean
----@field term_colors boolean
----@field ending_tildes boolean
----@field cmp_itemkind_reverse boolean
----@field code_style TerraConfig.CodeStyle
----@field colors table<string, string>
----@field highlights table<string, string>
----@field diagnostics TerraConfig.Diagnostics
-
----@class Colors
----@field primary Primary
----@field palette Palette
----@field semantic Semantic
----@field none string
-
----@alias HighlightGroupName string
-
----@alias HighlightFormat  "bold" | "underline" | "underlineline" | "undercurl" | "underdot" | "underdash" | "strikethrough" | "reverse" | "inverse" | "italic" | "standout" | "nocombine" | "NONE"
+---@alias HighlightFormat                         "bold" | "underline" | "underlineline" | "undercurl" | "underdot" | "underdash" | "strikethrough" | "reverse" | "inverse" | "italic" | "standout" | "nocombine" | "NONE"
 
 ---@class HighlightGroupSettings
----@field fg string
----@field bg string
----@field sp string
----@field fmt HighlightFormat
+---@field fg                                      string
+---@field bg                                      string
+---@field sp                                      string
+---@field fmt                                     HighlightFormat
 
----@alias HighlightGroup table<HighlightGroupName, HighlightGroupSettings>
+---@alias HighlightGroup                          table<HighlightGroupName, HighlightGroupSettings>
+
+-------------------------------------------Colors
+---@class Colors
+---@field primary                                 Primary
+---@field palette                                 Palette
+---@field semantic                                Semantic
+---@field none                                    string
+
+-------------------------------------------Config
+---@alias Season                                  "spring" | "summer" | "fall" | "winter"
+---@alias Time                                    "day" | "night"
+---@alias CodeStyleFormat                         HighlightFormat
+
+---@class TerraConfig.CodeStyle
+---@field comments                                CodeStyleFormat
+---@field keywords                                CodeStyleFormat
+---@field functions                               CodeStyleFormat
+---@field strings                                 CodeStyleFormat
+---@field variables                               CodeStyleFormat
+
+---@class TerraConfig.Diagnostics
+---@field darker                                  boolean
+---@field undercurl                               boolean
+---@field background                              boolean
+
+---@class TerraConfig
+---@field season                                  Season
+---@field time                                    Time
+---@field icons                                   table
+---@field select_season                           string
+---@field transparent                             boolean
+---@field term_colors                             boolean
+---@field ending_tildes                           boolean
+---@field cmp_itemkind_reverse                    boolean
+---@field code_style                              TerraConfig.CodeStyle
+---@field colors                                  table<string, string>
+---@field highlights                              table<string, string>
+---@field diagnostics                             TerraConfig.Diagnostics
