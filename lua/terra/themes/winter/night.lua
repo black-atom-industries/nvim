819eff31472875b23f8036063b7a7825fd313097
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-02-11 18:56:47 +0100

    feat(themes): update winter (and aligned others)
    squash themes

--- lua/terra/themes/winter/night.lua
+++ lua/terra/themes/winter/night.lua
@@ -5,40 +5,40 @@ local M = {}
 ---@type TerraPrimaryColors
 M.primary = {
     -- Lows
-    "#222831",
-    "#252d38",
-    "#2d3644",
-    "#323d50",
+    "#192125",
+    "#1d272c",
+    "#212c31",
+    "#24333a",
 
     -- Mids
-    "#47526a",
-    "#515d7a",
-    "#5b6a88",
-    "#7384a3",
+    "#475e6a",
+    "#516c7a",
+    "#5b7988",
+    "#7393a3",
 
     -- Highs
-    "#c0c8d6",
-    "#ced6e3",
-    "#dfe4ec",
-    "#eff2f6",
+    "#d3cec5",
+    "#dfdbd3",
+    "#e9e6e1",
+    "#f4f3f1",
 }
 
 ---@type TerraPaletteColors
 M.palette = {
     black = M.primary[4],
-    gray = M.primary[6],
+    gray = M.primary[5],
 
     dark_red = "#d9663f",
     red = "#e5744e",
 
     dark_green = "#4ca144",
-    green = "#85ce7e",
+    green = "#83C57D",
 
     dark_yellow = "#ffa564",
     yellow = "#ffcc69",
 
-    dark_blue = "#8eb9f2",
-    blue = "#69c6e5",
+    dark_blue = "#8fb7ef",
+    blue = "#78cfec",
 
     dark_magenta = "#c77bb9",
     magenta = "#bf9de2",
@@ -66,9 +66,9 @@ M.semantic = {
     },
     fg = {
         dark = M.primary[7],
-        main = M.primary[9],
-        light = M.primary[11],
-        active = M.palette.yellow,
+        main = M.primary[10],
+        light = M.primary[12],
+        active = M.palette.cyan,
         neutral = M.primary[5],
         diff = {
             add = M.palette.green,
