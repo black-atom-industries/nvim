f6e196d6fb5367be4c2fe42e415778863d476555
Nikolaus Brunner <nikolaus.brunner@protonmail.ch>
2023-04-07 13:04:32 +0200

    docs(actions): Expand comment for actions.ui.notify

--- lua/terra/actions/ui.lua
+++ lua/terra/actions/ui.lua
@@ -2,9 +2,14 @@ local M = {}
 
 ---Notification which conditionally calls `nvim-notify` when available
 ---@param message string Notification message
----@param level number Log level. See vim.log.levels
+---@param level number Log level - Use `vim.log.levels`
 ---@param opts? table<string, any> nvim-notify options. See notify.Options
 ---@return nil
+---@usage
+---```lua
+----- Example:
+---notify("Hello World", vim.log.levels.INFO, { title = "Hello" })
+---```
 function M.notify(message, level, opts)
     level = level or vim.log.levels.INFO
     opts = opts or {}
