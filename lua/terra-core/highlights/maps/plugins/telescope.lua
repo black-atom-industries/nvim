b2add0cca8cf825494643ec64297992055f4f0c1
nbr <nikolaus.brunner@protonmail.ch>
2023-10-17 20:37:02 +0200

    DEV-107 plugins(telescope): improve highlights

--- lua/terra-core/highlights/maps/plugins/telescope.lua
+++ lua/terra-core/highlights/maps/plugins/telescope.lua
@@ -3,14 +3,22 @@ local highlight_map_extension = {
     map = function(colors)
         ---@type TerraHighlights
         local highlights_map = {
-            TelescopeNormal = { bg = colors.semantics.bg.primary.dark },
-            TelescopeBorder = {
-                fg = colors.semantics.fg.neutral,
+            TelescopeNormal = {
                 bg = colors.semantics.bg.primary.dark,
             },
-            TelescopeTitle = { fg = colors.semantics.fg.active },
-            TelescopePromptPrefix = { fg = colors.semantics.fg.active },
-            TelescopeMatching = { fg = colors.semantics.fg.active },
+            TelescopeBorder = {
+                fg = colors.semantics.bg.primary.dark,
+                bg = colors.semantics.bg.primary.dark,
+            },
+            TelescopeTitle = {
+                fg = colors.semantics.fg.active,
+            },
+            TelescopePromptPrefix = {
+                fg = colors.semantics.fg.active,
+            },
+            TelescopeMatching = {
+                fg = colors.semantics.fg.active,
+            },
             TelescopeSelection = {
                 fg = colors.semantics.fg.active,
                 bg = colors.semantics.bg.primary.main,
