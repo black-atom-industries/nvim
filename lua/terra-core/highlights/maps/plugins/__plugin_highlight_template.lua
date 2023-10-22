f9b6a8aa2e0685e852cf79b5306c7ce03b3fb407
nbr <nikolaus.brunner@protonmail.ch>
2023-10-22 12:26:40 +0200

    DEV-108 plugins: update template

--- lua/terra-core/highlights/maps/plugins/__plugin_highlight_template.lua
+++ lua/terra-core/highlights/maps/plugins/__plugin_highlight_template.lua
@@ -1,26 +1,28 @@
 --[[
     Hello there!
-    Thank you for wanting to contribute to the Terra Theme core.nvim plugin!
+    Thank you for wanting to contribute to the terra-core.nvim plugin!
 
     To create a new highlight map for your plugin, simply copy this template and rename it to a suitable name, such as "my_plugin.lua".
 
     You don't need to require this file anywhere manually, since every lua file in the `lua/terra/highlights/maps/` folder will be loaded automatically.
     Note: Filenames ending with "_template.lua" will be disregarded!
 
-    Each highlight file must return a `TerraHighlightsSpec` table containing at least a field called `map`.
+    Each highlight file must return a `TerraHighlightsSpec` table, which at a minimum, should contain a field called `map`.
     The `TerraHighlightsSpec` and `TerraHighlights` types will assist with completion and type checking.
-    (Ensure the `sumneko.lua` language server installed for this to work.)
+    (Ensure the `lua-language-server` is installed for this to work.)
 
     The `enabled` field is optional and defaults to true.
-    The `map` function needs to return a `TerraHighlights` table. It receives the colors and config tables as parameters, which gives you access to all the colors and config options.
+    The `map` function needs to return a `TerraHighlights` table. It receives the colors and config tables as parameters,
+    which gives you access to all the colors and config options.
+    -- `colors` is a table containing the color palette.
+    -- `config` is a table containing configuration options.
 
-    After you have finished your highlight map, 
-    you can test it by running by running Neovim in a separate window 
-    and see if your highlights are applied correctly, 
-    or turn on `debug` mode inside "lua/terra/config.lua" to see a generated file with all highlights.
-    This will be placed in the `debug` folder inside the `core.nvim` plugin folder.
+    After you have finished your highlight map, you can test it by running Neovim in a separate window 
+    and see if your highlights are applied correctly, or turn on `debug` mode inside "lua/terra/config.lua" to see a generated file with all highlights.
+    This will be placed in the `debug` folder inside the `terra-core.nvim` plugin folder.
+
+    For further assistance or questions, consider reaching out through the project's GitHub repository.
 ]]
---
 
 ---@type TerraHighlightsSpec
 local highlight_map_spec = {
