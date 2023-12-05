8c952f6bec00e0b0684a944671a906e98973e6f2
nbr <nikolaus.brunner@protonmail.ch>
2023-12-05 11:24:13 +0100

    feat(ui): add WinSeparator hl group

--- lua/terra-core/highlights/ui.lua
+++ lua/terra-core/highlights/ui.lua
@@ -108,6 +108,7 @@ local highlight_map_extension = {
             TabLineFill = { fg = palette.gray, bg = bg.primary.main },
             TabLineSel = { fg = fg.primary.main, bg = bg.primary.light },
             VertSplit = { fg = bg.primary.dark, bg = bg.primary.main },
+            WinSeparator = { fg = bg.primary.dark, bg = bg.primary.main },
             Visual = { bg = bg.match.active },
             VisualNOS = { link = "Visual" },
             QuickFixLine = { fg = palette.blue, bg = bg.primary.light },
