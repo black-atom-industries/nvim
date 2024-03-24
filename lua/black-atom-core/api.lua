6aa5c33dd492860846292de3d2e3e9aeec0b4382
nbr <nikolaus.brunner@protonmail.ch>
2024-03-24 07:25:10 +0100

    refactor: rename util to api (DEV-135)

--- lua/black-atom-core/api.lua
+++ lua/black-atom-core/api.lua
@@ -1,6 +1,6 @@
 -- Public API for certain lib functions
 
----@class BlackAtomCore.Util
+---@class BlackAtomCore.API
 local M = {}
 
 M.select_theme = require("black-atom-core.lib.config").select_theme
