f117945db35956a78262476b62b09bffff797c63
nbr <nikolaus.brunner@protonmail.ch>
2024-09-14 12:14:40 +0200

    feat(ui): adjust WinSeparator

--- lua/black-atom/highlights/ui.lua
+++ lua/black-atom/highlights/ui.lua
@@ -78,7 +78,7 @@ return {
 
             WinBar = { link = "StatusLine" },
             WinBarNC = { link = "StatusLineNC" },
-            WinSeparator = { fg = ui.fg.neutral, bg = bg.main(config, colors) },
+            WinSeparator = { fg = bg.sidebar(config, colors), bg = bg.main(config, colors) },
 
             -- Folds & Columns
             Folded = { fg = ui.fg.neutral, bg = bg.main(config, colors) },
