terra-theme/terra-core.nvim

feat(themes): add CRBN collection with light theme variant
- feat: introduce new `crbn` collection with light theme variant
- style: implement high-contrast `primaries` with cool gray undertones
- style: create vibrant `palette` with bright accent colors
- style(lsp): update reference highlights to use underlines
- style(plugins): refine UI colors for better visual hierarchy

lua/black-atom/themes/crbn/ui_light.lua
@@ -1,0 +1,50 @@
+---@param primaries BlackAtom.Theme.Primaries
+---@param palette BlackAtom.Theme.Palette
+---@return BlackAtom.Theme.UI
+return function(primaries, palette)
+    ---@type BlackAtom.Theme.UI
+    return {
+        bg = {
+            primary = {
+                dark = primaries[7],
+                main = primaries[9],
+                light = primaries[8],
+            },
+            active = primaries[7],
+            match = {
+                active = palette.magenta,
+                passive = primaries[8],
+            },
+            diff = {
+                add = palette.green,
+                delete = palette.red,
+                change = palette.blue,
+                text = palette.dark_blue,
+            },
+        },
+        fg = {
+            primary = {
+                dark = primaries[1],
+                main = primaries[2],
+                light = primaries[3],
+            },
+            active = palette.yellow,
+            invert = primaries[8],
+            neutral = primaries[4],
+            diff = {
+                add = palette.green,
+                delete = palette.red,
+                change = palette.blue,
+                text = palette.dark_blue,
+            },
+        },
+        feedback = {
+            error = palette.red,
+            warn = palette.yellow,
+            info = palette.blue,
+            hint = palette.dark_yellow,
+            todo = palette.yellow,
+            success = palette.green,
+        },
+    }
+end

black-atom-industries/nvim

lua/black-atom/themes/crbn/ui_light.lua
@@ -1,0 +1,50 @@
+---@param primaries BlackAtom.Theme.Primaries
+---@param palette BlackAtom.Theme.Palette
+---@return BlackAtom.Theme.UI
+return function(primaries, palette)
+    ---@type BlackAtom.Theme.UI
+    return {
+        bg = {
+            primary = {
+                dark = primaries[8],
+                main = primaries[9],
+                light = primaries[7],
+            },
+            active = primaries[6],
+            match = {
+                active = palette.magenta,
+                passive = primaries[8],
+            },
+            diff = {
+                add = palette.green,
+                delete = palette.red,
+                change = palette.blue,
+                text = palette.dark_blue,
+            },
+        },
+        fg = {
+            primary = {
+                dark = primaries[1],
+                main = primaries[2],
+                light = primaries[3],
+            },
+            active = palette.yellow,
+            invert = primaries[8],
+            neutral = primaries[4],
+            diff = {
+                add = palette.green,
+                delete = palette.red,
+                change = palette.blue,
+                text = palette.dark_blue,
+            },
+        },
+        feedback = {
+            error = palette.red,
+            warn = palette.yellow,
+            info = palette.blue,
+            hint = palette.dark_yellow,
+            todo = palette.yellow,
+            success = palette.green,
+        },
+    }
+end