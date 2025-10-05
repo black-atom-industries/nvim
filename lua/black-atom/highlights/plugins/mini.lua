714968162b8f5671916c1feb832e8464ee2e0c06
nbr <nikolaus.brunner@protonmail.ch>
2025-10-05 14:25:29 +0200

    feat(highlights): add mini.nvim plugin highlights

--- lua/black-atom/highlights/plugins/mini.lua
+++ lua/black-atom/highlights/plugins/mini.lua
@@ -4,12 +4,24 @@ return {
     enabled = true,
     map = function(colors)
         local ui = colors.ui
-        local palette = colors.palette
 
         ---@type BlackAtom.Highlights
         return {
             MiniPickPrompt = { fg = ui.fg.accent, bg = ui.bg.float },
             MiniFloatBorder = { fg = ui.fg.accent, bg = ui.bg.float },
+
+            -- MiniDiff: Sign highlights
+            MiniDiffSignAdd = { fg = ui.fg.add },
+            MiniDiffSignChange = { fg = ui.fg.modify },
+            MiniDiffSignDelete = { fg = ui.fg.delete },
+
+            -- MiniDiff: Overlay highlights
+            MiniDiffOverAdd = { bg = ui.bg.selection },
+            MiniDiffOverChange = { fg = ui.fg.modify, bg = ui.bg.active },
+            MiniDiffOverChangeBuf = { fg = ui.fg.info, bg = ui.bg.active },
+            MiniDiffOverContext = { fg = ui.fg.subtle },
+            MiniDiffOverContextBuf = { fg = ui.fg.default },
+            MiniDiffOverDelete = { fg = ui.fg.delete, bg = ui.bg.active },
         }
     end,
 }
