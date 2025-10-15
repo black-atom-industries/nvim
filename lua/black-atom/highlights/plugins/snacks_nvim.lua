ea19fdcda433fffbeae0b1d15e3ee08ef35b24ab
nbr <nikolaus.brunner@protonmail.ch>
2025-10-15 18:11:35 +0200

    feat(highlights): add snacks.nvim plugin highlight groups

--- lua/black-atom/highlights/plugins/snacks_nvim.lua
+++ lua/black-atom/highlights/plugins/snacks_nvim.lua
@@ -2,11 +2,16 @@
 ---@type BlackAtom.HighlightsSpec
 return {
     enabled = true,
-    map = function(colors, config)
+    map = function(colors)
         local ui = colors.ui
 
         ---@type BlackAtom.Highlights
         return {
+            SnacksPickerBorder = { fg = ui.fg.accent, bg = ui.bg.float },
+            SnacksPickerPreviewBorder = { fg = ui.bg.default, bg = ui.bg.default },
+            SnacksPickerMatch = { fg = ui.fg.accent },
+            SnacksPickerFile = { fg = ui.fg.default, bold = true },
+            SnacksPickerDir = { fg = ui.fg.subtle },
             SnacksNotifierHistory = { bg = ui.bg.float },
             SnacksPickerCursorLine = { bg = ui.bg.active },
             SnacksPickerListCursorLine = { bg = ui.bg.default },
